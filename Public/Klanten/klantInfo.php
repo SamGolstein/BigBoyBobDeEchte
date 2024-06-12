0c7570874d503beb9963cdab1dc2e5449441ec8a
Chris-26 <chris.halfman@student.graafschapcollege.nl>
2024-06-12 13:11:24 +0200

    frontend10

--- Public/Klanten/klantInfo.php
+++ Public/Klanten/klantInfo.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../style/klantInfo.css">
 </head>
+
 <body>
+    <nav class="navBar">
+        <img src="../../img/BigBoyBobLogo.png" alt="BigBoyBobLogo">
+        <ul>
+            <li><a href="../Login/index.php">Home</a></li>
+            <li><a href="../Klanten/index.php">Klanten</a></li>
+            <li><a href="../Factuur/index.php">facturen</a></li>
+        </ul>
+        <a href="" class="accountButton">Account</a>
+    </nav>
     <h2>Klant info</h2>
 </body>
+
 </html>
