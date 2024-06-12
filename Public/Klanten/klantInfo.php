7f4c964f276ca2f7ce3cb37184a38b3c0cdcc705
Chris-26 <chris.halfman@student.graafschapcollege.nl>
2024-06-12 13:39:56 +0200

    frontend11

--- Public/Klanten/klantInfo.php
+++ Public/Klanten/klantInfo.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+include '../../src/src/klanten.php';
+include '../../src/src/factuur.php';
+
+$klant = new Klanten();
+$factuur = new Factuur();
+
+$klantInfo = $klant->getKlant($_GET['id']);
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +28,25 @@
         </ul>
         <a href="" class="accountButton">Account</a>
     </nav>
-    <h2>Klant info</h2>
+
+    <div class="container">
+        <?php
+        foreach ($klantInfo as $klant) {
+            ?>
+            <div class="klantInfo">
+                <h2>Klant info</h2>
+                <p>Voornaam: <?php echo $klant['voornaam']; ?></p>
+                <p>Achternaam: <?php echo $klant['achternaam']; ?></p>
+                <p>Straat: <?php echo $klant['straat']; ?></p>
+                <p>Postcode: <?php echo $klant['postcode']; ?></p>
+                <p>Woonplaats: <?php echo $klant['woonplaats']; ?></p>
+                <p>Telefoonnummer: <?php echo $klant['telefoonnummer']; ?></p>
+                <p>Email: <?php echo $klant['email']; ?></p>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
 </body>
 
 </html>
