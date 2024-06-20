f0b96c9bfe6b8c1d0096918b7f4d62fc7cfaa982
Chris-26 <chris.halfman@student.graafschapcollege.nl>
2024-06-20 12:46:52 +0200

    Kleine fixes

--- Public/Factuur/bekijken.php
+++ Public/Factuur/bekijken.php
@@ -31,107 +31,115 @@ $klantInfo = $klant->getKlant($klantId[0]['klant_id']);
         </ul>
         <a href="account.php" class="accountButton">Account</a>
     </nav>
-    <a href='index.php'>Terug</a>
 
-    <h1>Factuur nummer <?php echo $_GET['id']; ?></h1>
-    <table border="1">
-        <th>Omschrijving</th>
-        <th>Uren</th>
-        <th>Prijs</th>
+    <div class="container">
+        <a href='index.php'>Terug</a>
 
-        <?php
-        $factuurDetails = $factuurRegel->getFactuurRegelById($_GET['id']);
-        foreach ($factuurDetails as $factuurDetail) {
-            echo "<tr>";
-            echo "<td>" . $factuurDetail['omschrijving'] . "</td>";
-            echo "<td>" . $factuurDetail['uren'] . "</td>";
-            $formattedPrice = number_format($factuurDetail['prijs'], 2, ',', '.');
-            echo "<td>€" . $formattedPrice . "</td>";
-            echo "</tr>";
-        }
-        ?>
-    </table>
-    <p><?php echo "Totaal bedrag: " . $factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag']; ?></p>
-    <form method="POST">
-        <input type="text" name="omschrijving" placeholder="Omschrijving">
-        <input type="text" name="aantal" placeholder="Uren">
-        <input type="text" name="prijs" placeholder="Prijs">
-        <input type="submit" name="submit" value="Toevoegen">
-    </form>
-    <button id="button">Maak factuur</button>
-    <div class="factuur" id="maakPDF">
-        <div class="factuurLogo">
-            <img src="../../img/BigBoyBobLogo.png" alt="BigBoyBobLogo">
-            <div class="bedrijfsInfo">
-                <p>BigBoyBob</p>
-                <p>J.F. Kennedylaan 49</p>
-                <p>7001 EA Doetinchem</p>
-                <p>KvK: 12345678</p>
-            </div>
-        </div>
-        <div class="klantInfo">
+        <h1>Factuur nummer <?php echo $_GET['id']; ?></h1>
+        <table class="table">
+            <th>Omschrijving</th>
+            <th>Uren</th>
+            <th>Prijs</th>
+
             <?php
-            foreach ($klantInfo as $klant) {
-                echo "<p>" . $klant['voornaam'] . " " . $klant['achternaam'] . "</p>";
-                echo "<p>" . $klant['straat'] . "</p>";
-                echo "<p>" . $klant['postcode'] . " " . $klant['woonplaats'] . "</p>";
+            $factuurDetails = $factuurRegel->getFactuurRegelById($_GET['id']);
+            foreach ($factuurDetails as $factuurDetail) {
+                echo "<tr>";
+                echo "<td>" . $factuurDetail['omschrijving'] . "</td>";
+                echo "<td>" . $factuurDetail['uren'] . "</td>";
+                $formattedPrice = number_format($factuurDetail['prijs'], 2, ',', '.');
+                echo "<td>€" . $formattedPrice . "</td>";
+                echo "</tr>";
             }
             ?>
-        </div>
-        <div class="factuurInfo">
-            <h2>FACTUUR</h2>
-            <table cellspacing="0">
-                <th>Uren</th>
-                <th>Omschrijving</th>
-                <th>Bedrag</th>
-                <th>Btw</th>
+            <td style="border: 0px; visibility: hidden;"></td>
+            <td style="border: 0px; visibility: hidden;"></td>
 
+            <td style="font-weight: bold; border-top: 2px solid black;">
+                <p><?php echo "Totaal bedrag: €" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'], 2, ',', '.'); ?></p>
+            </td>
+        </table>
+        <form method="POST">
+            <input type="text" name="omschrijving" placeholder="Omschrijving" required>
+            <input type="text" name="aantal" placeholder="Uren" required>
+            <input type="text" name="prijs" placeholder="Prijs" required>
+            <input type="submit" name="submit" value="Toevoegen">
+        </form>
+        <button id="button">Maak factuur</button>
+        <div class="factuur" id="maakPDF">
+            <div class="factuurLogo">
+                <img src="../../img/BigBoyBobLogo.png" alt="BigBoyBobLogo">
+                <div class="bedrijfsInfo">
+                    <p>BigBoyBob</p>
+                    <p>J.F. Kennedylaan 49</p>
+                    <p>7001 EA Doetinchem</p>
+                    <p>KvK: 12345678</p>
+                </div>
+            </div>
+            <div class="klantInfo">
                 <?php
-                $factuurDetails = $factuurRegel->getFactuurRegelById($_GET['id']);
-                foreach ($factuurDetails as $factuurDetail) {
-                    echo "<tr>";
-                    echo "<td>" . $factuurDetail['uren'] . "</td>";
-                    echo "<td>" . $factuurDetail['omschrijving'] . "</td>";
-                    $formattedPrice = number_format($factuurDetail['prijs'], 2, ',', '.');
-                    echo "<td>€" . $formattedPrice . "</td>";
-                    echo "<td>21%</td>";
-                    echo "</tr>";
+                foreach ($klantInfo as $klant) {
+                    echo "<p>" . $klant['voornaam'] . " " . $klant['achternaam'] . "</p>";
+                    echo "<p>" . $klant['straat'] . "</p>";
+                    echo "<p>" . $klant['postcode'] . " " . $klant['woonplaats'] . "</p>";
                 }
                 ?>
-                <td style="border-bottom: 0px"></td>
-                <td>Totaal exclusief BTW</td>
-                <td><?php echo "€" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'], 2, ',', '.'); ?>
-                </td>
-                <tr></tr>
-                <td style="border-bottom: 0px"></td>
-                <td>BTW(21%)</td>
-                <td><?php echo "€" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'] * 0.21, 2, ',', '.'); ?>
-                </td>
-                <tr></tr>
-                <td style="border-bottom: 0px"></td>
-                <td style="font-weight: bold; border-bottom: none">Totaal inclusief BTW</td>
-                <td style="font-weight: bold; border-bottom: none">
-                    <?php echo "€" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'] * 1.21, 2, ',', '.'); ?>
-                </td>
-            </table>
+            </div>
+            <div class="factuurInfo">
+                <h2>FACTUUR</h2>
+                <table cellspacing="0">
+                    <th>Uren</th>
+                    <th>Omschrijving</th>
+                    <th>Bedrag</th>
+                    <th>Btw</th>
+
+                    <?php
+                    $factuurDetails = $factuurRegel->getFactuurRegelById($_GET['id']);
+                    foreach ($factuurDetails as $factuurDetail) {
+                        echo "<tr>";
+                        echo "<td>" . $factuurDetail['uren'] . "</td>";
+                        echo "<td>" . $factuurDetail['omschrijving'] . "</td>";
+                        $formattedPrice = number_format($factuurDetail['prijs'], 2, ',', '.');
+                        echo "<td>€" . $formattedPrice . "</td>";
+                        echo "<td>21%</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                    <td style="border-bottom: 0px"></td>
+                    <td>Totaal exclusief BTW</td>
+                    <td><?php echo "€" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'], 2, ',', '.'); ?>
+                    </td>
+                    <tr></tr>
+                    <td style="border-bottom: 0px"></td>
+                    <td>BTW(21%)</td>
+                    <td><?php echo "€" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'] * 0.21, 2, ',', '.'); ?>
+                    </td>
+                    <tr></tr>
+                    <td style="border-bottom: 0px"></td>
+                    <td style="font-weight: bold; border-bottom: none">Totaal inclusief BTW</td>
+                    <td style="font-weight: bold; border-bottom: none">
+                        <?php echo "€" . number_format($factuur->getTotaalBedrag($_GET['id'])[0]['totaal_bedrag'] * 1.21, 2, ',', '.'); ?>
+                    </td>
+                </table>
+            </div>
+            <p style="margin-top: 50px">Gelieve binnen 14 dagen te betalen op NL28ABNA000000000000</p>
         </div>
-        <p style="margin-top: 50px">Gelieve binnen 14 dagen te betalen op NL28ABNA000000000000</p>
+        <?php
+        if (isset($_POST['submit'])) {
+            $factuurRegel->setFactuurnr($_GET['id']);
+            $factuurRegel->setOmschrijving($_POST['omschrijving']);
+            $factuurRegel->setAantal($_POST['aantal']);
+            $factuurRegel->setPrijs($_POST['prijs']);
+
+            $totaal = $_POST['aantal'] * $_POST['prijs'];
+
+            $factuur->updateTotaalBedrag($totaal, $_GET['id']);
+
+            $factuurRegel->saveRegel();
+            header("Location: bekijken.php?id=" . $_GET['id']);
+        }
+        ?>
     </div>
-    <?php
-    if (isset($_POST['submit'])) {
-        $factuurRegel->setFactuurnr($_GET['id']);
-        $factuurRegel->setOmschrijving($_POST['omschrijving']);
-        $factuurRegel->setAantal($_POST['aantal']);
-        $factuurRegel->setPrijs($_POST['prijs']);
-
-        $totaal = $_POST['aantal'] * $_POST['prijs'];
-
-        $factuur->updateTotaalBedrag($totaal, $_GET['id']);
-
-        $factuurRegel->saveRegel();
-        header("Location: bekijken.php?id=" . $_GET['id']);
-    }
-    ?>
 </body>
 <script>
     window.document.onload = function () {
--- Public/Factuur/index.php
+++ Public/Factuur/index.php
@@ -1,10 +1,9 @@
 <?php
-require_once('../../config/db_config.php');
-require_once('../../src/src/factuur.php'); 
-require_once('../../src/src/klanten.php');
+require_once ('../../config/db_config.php');
+require_once ('../../src/src/factuur.php');
+require_once ('../../src/src/klanten.php');
 
-if($_SESSION['gebruikersnaam'] == null)
-{
+if ($_SESSION['gebruikersnaam'] == null) {
     header('Location: ../Login/index.php');
 }
 
@@ -16,28 +15,43 @@ $klant = new Klanten();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facturen</title>
-    <link rel="stylesheet" href="../../style/index.css">
+    <link rel="stylesheet" href="../../style/factuurIndex.css">
 </head>
-<body>
-    <h1>Facturen</h1>
-    <table border="1">
-        <th>Factuur nummer</th>
-        <th>Klant naam</th>
-    <?php
-    foreach ($alleFacturen as $factuur) {
-        echo "<tr>";
-        echo "<td> <a href='bekijken.php?id=" . $factuur['factuur_id'] . "'>" . $factuur['factuur_id'] . "</td></a>";        
-        $klantNaam = $klant->getKlant($factuur['klant_id']);
-        
-        echo "<td>" . $klantNaam[0]['voornaam'] . "</td>";
-        echo "</tr>";
-    }
-    ?>
-    </table>
 
+<body>
+    <nav class="navBar">
+        <img src="../../img/BigBoyBobLogo.png" alt="BigBoyBobLogo">
+        <ul>
+            <li><a href="../Login/index.php">Home</a></li>
+            <li><a href="../Klanten/index.php">Klanten</a></li>
+            <li><a href="../Factuur/index.php">facturen</a></li>
+        </ul>
+        <a href="account.php" class="accountButton">Account</a>
+    </nav>
+
+    <div class="container">
+        <a href='../Klanten/index.php'>Terug</a>
+        <h1>Facturen</h1>
+        <table>
+            <th>Factuur nummer</th>
+            <th>Klant naam</th>
+            <?php
+            foreach ($alleFacturen as $factuur) {
+                echo "<tr>";
+                echo "<td> <a href='bekijken.php?id=" . $factuur['factuur_id'] . "'>" . $factuur['factuur_id'] . "</td></a>";
+                $klantNaam = $klant->getKlant($factuur['klant_id']);
+
+                echo "<td>" . $klantNaam[0]['voornaam'] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </div>
 </body>
+
 </html>
